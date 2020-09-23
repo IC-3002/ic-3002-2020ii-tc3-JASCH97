a5c43b2da3497b045ddccd19dd6a8c5baaef56fc
Joan Sanchez <joansc16@outlook.com>
2020-09-22 22:35:09 -0600

    Se crea la funcion cubica y constante de la tarea corta #3

--- sumatoria.py
+++ sumatoria.py
@@ -1,6 +1,16 @@
 def sumatoria_cubica(n):
-    raise NotImplementedError()
+	resultado = 0
+
+	for i in range(1,n+1):                                
+		for j in range(1,i+1):
+			for k in range(j,(i+j)+1):
+				resultado +=1
+				
+	return resultado
+				
 
 def sumatoria_constante(n):
-    raise NotImplementedError()
+	resultado = int(((n*(n+1)*(n+2))/3))
+	return resultado
+	
 
